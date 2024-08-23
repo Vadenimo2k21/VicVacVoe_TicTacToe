2ab5c680ca930adbd69206604a7a80eca15bfb69
Vadenimo <koop47@googlemail.com>
2024-08-23 21:27:57 +0200

    Add files via upload

--- main/main.cpp
+++ main/main.cpp
@@ -4,14 +4,6 @@
 
 char field[10];
 
-void singleplayer();
-void multiplayer();
-void help();
-bool checkFull();
-bool checkWin(char);
-char keyInput(bool x);
-void CPU(int);
-
 void consoleClear() {
     system("cls");
     //system("clear");
@@ -20,29 +12,6 @@ void consoleClear() {
 void fieldReset() {
     for (int i = 0; i < 10; i++) {
         field[i] = ' ';
-    }
-}
-
-void menu() {
-    while (true) {
-        fieldReset();
-        char inputmenu;
-
-        std::cout << "\n\n";
-        std::cout << "GAME MENU:\n\n";
-        std::cout << "1: Singleplayer\n";
-        std::cout << "2: Local Multiplayer\n";
-        std::cout << "3: Help\n";
-        std::cout << "4: Quit\n\n";
-        std::cout << "Input: ";
-
-        std::cin >> inputmenu;
-
-        if (inputmenu == '1') singleplayer();
-        else if (inputmenu == '2') multiplayer();
-        else if (inputmenu == '3') help();
-        else if (inputmenu == '4' || inputmenu == 'q' || inputmenu == 'Q') return;
-        else { consoleClear(); std::cout << "Input invalid.\nPlease try again.\n"; }
     }
 }
 
@@ -167,6 +136,29 @@ bool checkFull() {
     return true;
 }
 
+void menu() {
+    while (true) {
+        fieldReset();
+        char inputmenu;
+
+        std::cout << "\n\n";
+        std::cout << "GAME MENU:\n\n";
+        std::cout << "1: Singleplayer\n";
+        std::cout << "2: Local Multiplayer\n";
+        std::cout << "3: Help\n";
+        std::cout << "4: Quit\n\n";
+        std::cout << "Input: ";
+
+        std::cin >> inputmenu;
+
+        if (inputmenu == '1') singleplayer();
+        else if (inputmenu == '2') multiplayer();
+        else if (inputmenu == '3') help();
+        else if (inputmenu == '4' || inputmenu == 'q' || inputmenu == 'Q') return;
+        else { consoleClear(); std::cout << "Input invalid.\nPlease try again.\n"; }
+    }
+}
+
 void CPU(int difficulty) {
     if (checkFull()) return;
     srand(time(nullptr));
@@ -246,109 +238,6 @@ void CPU(int difficulty) {
         for (int i = 1; i < 10; i++) {
             if (field[i] == ' ') {
                 field[i] = 'X'; 
-                if (checkWin('X')) {
-                    field[i] = 'O';
-                    return;
-                }
-                else field[i] = ' ';
-            }
-        }
-        int middleField = rand() % 4;
-        if (field[5] == ' ' && middleField != 1) {
-            field[5] = 'O';
-            return;
-        }
-        while (true) {
-            srand(time(nullptr));
-            int i = rand() % 9 + 1;
-            if (field[i] == ' ') {
-                field[i] = 'O';
-                return;
-            }
-        }
-    }
-    else return;
-}
-
-void CPU(int difficulty) {
-    if (checkFull()) return;
-    srand(time(nullptr));
-    if (difficulty == 1) {
-        // Logik für Easy.
-        int check = rand() % 3;
-        if (check != 1) {
-            for (int i = 1; i < 10; i++) {
-                if (field[i] == ' ') {
-                    field[i] = 'O';
-                    if (checkWin('O')) return;
-                    else field[i] = ' ';
-                }
-            }
-        }
-        if (check != 2) {
-            for (int i = 1; i < 10; i++) {
-                if (field[i] == ' ') {
-                    field[i] = 'X';
-                    if (checkWin('X')) {
-                        field[i] = 'O';
-                        return;
-                    }
-                    else field[i] = ' ';
-                }
-            }
-        }
-        while (true) {
-            int i = rand() % 9 + 1;
-            if (field[i] == ' ') {
-                field[i] = 'O';
-                return;
-            }
-        }
-    }
-    if (difficulty == 2) {
-        // Logik für Medium.
-        int check = rand() % 5 + 1;
-        if (check != 3) {
-            for (int i = 1; i < 10; i++) {
-                if (field[i] == ' ') {
-                    field[i] = 'O';
-                    if (checkWin('O')) return;
-                    else field[i] = ' ';
-                }
-            }
-        }
-        else if (check != 4) {
-            for (int i = 1; i < 10; i++) {
-                if (field[i] == ' ') {
-                    field[i] = 'X';
-                    if (checkWin('X')) {
-                        field[i] = 'O';
-                        return;
-                    }
-                    else field[i] = ' ';
-                }
-            }
-        }
-        while (true) {
-            int i = rand() % 9 + 1;
-            if (field[i] == ' ') {
-                field[i] = 'O';
-                return;
-            }
-        }
-    }
-    if (difficulty == 3) {
-        // Logik für Hard.
-        for (int i = 1; i < 10; i++) {
-            if (field[i] == ' ') {
-                field[i] = 'O';
-                if (checkWin('O')) return;
-                else field[i] = ' ';
-            }
-        }
-        for (int i = 1; i < 10; i++) {
-            if (field[i] == ' ') {
-                field[i] = 'X';
                 if (checkWin('X')) {
                     field[i] = 'O';
                     return;
